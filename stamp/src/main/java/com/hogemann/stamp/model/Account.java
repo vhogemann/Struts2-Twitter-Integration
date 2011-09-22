54340ebb0f1e27386a5c952837ac7c63648b94c5
Victor Högemann <victor.hogemann@gmail.com>
2011-09-22 18:24:54 -0300

    Classic... forgot to add a file. Still figuring out how this eclipse-git plugin works

---
+++ stamp/src/main/java/com/hogemann/stamp/model/Account.java
@@ -0,0 +1,40 @@
+package com.hogemann.stamp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+public class Account extends AbstractEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@ManyToOne
+	private User user;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}
